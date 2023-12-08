f412f144ca4f60cc439c6e76d48a7ae48b8ed2c7
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-12-08 13:58:59 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" exact element={Home} />
-      <Route path="/catalog" element={Catalog} />
-      <Route path="/favorites" element={Favorites} />
+      <Route path="/" />
+      <Route path="/catalog" />
+      <Route path="/favorites" />
     </Routes>
   );
 };
