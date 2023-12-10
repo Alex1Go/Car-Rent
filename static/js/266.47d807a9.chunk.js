89def68a94772538cd18ccddc9b1015f13083435
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-12-10 22:28:03 +0000

    Deploying to gh-pages from @ Alex1Go/Car-Rent@69e029f85ba862d6c4e16ecf7791220fabbcd3b7 🚀

---
+++ static/js/266.47d807a9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rent=self.webpackChunkcar_rent||[]).push([[266],{1266:function(e,n,r){r.r(n);var c=r(184);n.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h2",{children:"Poiu"})})}}}]);
+//# sourceMappingURL=266.47d807a9.chunk.js.map
